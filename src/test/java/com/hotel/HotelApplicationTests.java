f5ce041e89fa989552ee075bfd0b50eece62ecd3
Dalest <73247972+Alex236623@users.noreply.github.com>
2023-06-10 17:28:27 +0300

    Refactoring

--- src/test/java/com/hotel/HotelApplicationTests.java
+++ src/test/java/com/hotel/HotelApplicationTests.java
@@ -1,12 +1,19 @@
 package com.hotel;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class HotelApplicationTests {
 
     @Test
     void contextLoads() {
+        // This test simply checks if the application context loads successfully
+        ConfigurableApplicationContext context = SpringApplication.run(HotelApplication.class);
+        assertNotNull(context);
     }
 }
